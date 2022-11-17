617fb03bb88bdece9b659895e3b950c4692b2ea5
dilmurod <dilmurod8829@gmail.com>
2022-11-17 22:03:53 +0200

    JS 3-oy 1-dars

---
+++ app.js
@@ -0,0 +1,17 @@
+let ism = prompt(" Ismingiz:");
+let familya = prompt("Familyangiz:");
+let yosh = prompt("Yoshingiz: ");
+let shahar = prompt("Yashash Manzilingiz: ");
+let tel = prompt("Telefon raqamingiz");
+let email = prompt("Elektron poctangiz @ :");
+let card = prompt("Karta Raqamingiz :");
+let bank = prompt("Bankdagi Hisob Raqamingiz");
+
+alert(`Ismingiz : ${ism} \n
+Familyangiz : ${familya} \n
+Yoshingiz : ${yosh} \n 
+Yashash Manzilingiz : ${shahar} \n
+Telfon Raqamingiz : ${tel} \n
+Elektron poctangiz@ :${email} \n
+Karta Raqamingiz : ${card} \n
+Bankdagi Hisob Raqamingiz :${bank}`);
